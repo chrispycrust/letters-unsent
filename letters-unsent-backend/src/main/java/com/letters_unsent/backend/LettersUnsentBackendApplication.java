6431691689baecb399459c9f06a2de5e871ea849
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-06-08 12:01:30 +1000

    initial commit

---
+++ letters-unsent-backend/src/main/java/com/letters_unsent/backend/LettersUnsentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.letters_unsent.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettersUnsentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettersUnsentBackendApplication.class, args);
+	}
+
+}
---
+++ letters-unsent-backend/src/test/java/com/letters_unsent/backend/LettersUnsentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letters_unsent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettersUnsentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d31c47b0e3700d5ed20157d677ea8cbb9b32d0fa
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-08-27 18:15:54 +1000

    add comments for future

--- frontend-react/.storybook/main.js
+++ frontend-react/.storybook/main.js
@@ -2,15 +2,19 @@
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
+    "@storybook/addon-onboarding", // can get rid of this after install 
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    // themes for dark mode support 
+    // a11ygit 
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/react-vite", // should i use webpack or ember etc
     options: {},
   },
+
+  // telemetry?
 };
 export default config;

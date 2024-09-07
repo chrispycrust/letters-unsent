c91682559a889f857ef278bc2c6527b16ebd781b
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-09-07 13:08:52 +1000

    update package name

--- backend-java/src/main/java/com/LettersUnsent/LettersUnsentBackendApplication.java
+++ backend-java/src/main/java/com/LettersUnsent/LettersUnsentBackendApplication.java
@@ -1,4 +1,4 @@
-package com.letters_unsent.backend;
+package com.LettersUnsent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

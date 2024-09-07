2b8d279b7e64c16579172653f3ecdc32f9fb0f3a
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-09-07 13:08:03 +1000

    update field names, add javadoc comments

--- backend-java/src/main/java/com/LettersUnsent/model/Letter.java
+++ backend-java/src/main/java/com/LettersUnsent/model/Letter.java
@@ -5,7 +5,11 @@ import java.util.Optional;
 
 public class Letter {
 	
-	// letterId
+	/**
+	 * Final because id is assigned, it can't be changed
+	 */
+	private final long letterId;
+	
 	// foreign key writerId, also optional
 	// foreign key letterStatusId
 	
@@ -14,34 +18,62 @@ public class Letter {
 	// stored as Text annotation - long format in database
 	private String content;
 	
-	// if user wants to manually set a date for the letter e.g. backdating purposes
-	// change to setDate
-	private LocalDateTime writtenDate;
 	
-	// nullable annotation?
-	// when letter was published to platform
-	// maybe change to submittedDate
+	/**
+	 * Stores date if user chooses to manually set a date for a letter (e.g. backdating purposes)
+	 * Should override submittedDate in display if this is not null
+	 * Nullable
+	 */
+	
+	private LocalDateTime setDate;
+	
+	
+	/**
+	 * Stores date when letter was published to platform.
+	 * Not displayed if setDate isn't null
+	 */
+	
 	private LocalDateTime submittedDate;
-	// necessary if a user has an account and saved their letter in a draft status
+	
+	
+	/**
+	 * necessary if a user has an account and saved their letter in a draft status and may want to sort letter by this feature
+	 * might i want to locate a letter by these times as well?
+	 */
+
 	private LocalDateTime createdDate;
 
 	private LocalDateTime updatedDate;
 	
-	// constructor
-	public Letter(String intendedRecipient, String content, LocalDateTime writtenDate,
+	public Letter(long letterId, String intendedRecipient, String content, LocalDateTime setDate,
 			LocalDateTime submittedDate, LocalDateTime createdDate, LocalDateTime updatedDate) {
 		super();
+		this.letterId = letterId;
 		this.intendedRecipient = intendedRecipient;
 		this.content = content;
-		this.writtenDate = writtenDate;
+		this.setDate = setDate;
 		this.submittedDate = submittedDate;
 		this.createdDate = createdDate;
 		this.updatedDate = updatedDate;
 	}
-
+	
 	// GETTERS & SETTERS
 	
-	// Recipient
+	/**
+	 * letter ID
+	 */
+	
+	public long getLetterId() {
+		return letterId;
+	}
+
+//	public void setLetterId(long letterId) {
+//		this.letterId = letterId;
+//	}
+	
+	/**
+	 * @return letter recipient
+	 */
 
 	public String getIntendedRecipient() {
 		return intendedRecipient;
@@ -63,12 +95,12 @@ public class Letter {
 	
 	// WRITTEN DATE
 	
-	public Optional<LocalDateTime> getWrittenDate() {
-		return Optional.ofNullable(writtenDate);
+	public Optional<LocalDateTime> getsetDate() {
+		return Optional.ofNullable(setDate);
 	}
 
-	public void setWrittenDate(LocalDateTime writtenDate) {
-		this.writtenDate = writtenDate;
+	public void setsetDate(LocalDateTime setDate) {
+		this.setDate = setDate;
 	}
 	
 	// PUBLISHED DATE
@@ -100,5 +132,6 @@ public class Letter {
 	public void setUpdatedDate(LocalDateTime updatedDate) {
 		this.updatedDate = updatedDate;
 	}
+	
 
 }

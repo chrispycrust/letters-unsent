f41ecd3b3cafb073e21c6fefb9692d1091b5cd87
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-09-28 05:52:24 +1000

    add javadoc comments, date fields, writer constructor for jpa

--- backend-java/src/main/java/com/LettersUnsent/model/Writer.java
+++ backend-java/src/main/java/com/LettersUnsent/model/Writer.java
@@ -1,17 +1,33 @@
 package com.LettersUnsent.model;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/**
+ * A person can choose to make an account to review and store their letter.
+ * 
+ * @author Christine Nguyen
+ */
+
+@Entity
 public class Writer {
 	
+	@Id
 	public long writerId;
 	public String username;
 	public String password;
 	public String email;
-	public LocalDateTime createdDate;
-	public LocalDateTime updatedDate;
+	public ZonedDateTime createdDate;
+	public ZonedDateTime updatedDate;
 	
-	public Writer(long writerId, String username, String password, String email, LocalDateTime createdDate, LocalDateTime updatedDate) {
+	protected Writer() {
+		
+	}
+	
+	public Writer(long writerId, String username, String password, String email, ZonedDateTime createdDate, ZonedDateTime updatedDate) {
 		super();
 		this.writerId = writerId;
 		this.username = username;
@@ -20,13 +36,19 @@ public class Writer {
 		this.createdDate = createdDate;
 		this.updatedDate = updatedDate;
 	}
+	
+	/*
+	 * -----------------------------------------------------------------------------------------------
+	 * GETTERS & SETTERS
+	 * -----------------------------------------------------------------------------------------------
+	 */
 
 	public long getWriterId() {
 		return writerId;
 	}
 	
 	// can't think of an instance where a writerId would need to be set
-	// if a writer needs to change their id it's usually an indicator that their remaking their account?
+	// if a writer needs to change their id it's usually an indicator that they're remaking their account?
 	public void setWriterId(long writerId) {
 		this.writerId = writerId;
 	}
@@ -55,19 +77,19 @@ public class Writer {
 		this.email = email;
 	}
 
-	public LocalDateTime getCreatedDate() {
+	public ZonedDateTime getCreatedDate() {
 		return createdDate;
 	}
 
-	public void setCreatedDate(LocalDateTime createdDate) {
+	public void setCreatedDate(ZonedDateTime createdDate) {
 		this.createdDate = createdDate;
 	}
 
-	public LocalDateTime getUpdatedDate() {
+	public ZonedDateTime getUpdatedDate() {
 		return updatedDate;
 	}
 
-	public void setUpdatedDate(LocalDateTime updatedDate) {
+	public void setUpdatedDate(ZonedDateTime updatedDate) {
 		this.updatedDate = updatedDate;
 	}
 

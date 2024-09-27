ac59cca664a061f45f776e31917b26936391ed87
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-09-28 06:02:14 +1000

    add reminder comments

--- backend-java/src/main/java/com/LettersUnsent/service/LetterService.java
+++ backend-java/src/main/java/com/LettersUnsent/service/LetterService.java
@@ -4,5 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LetterService {
+	
+	// create letter 
+	
+	// retrieve letter
+	
+	// delete letter by id
+	
+	// save all letters 
+	
+	// update letter
 
 }

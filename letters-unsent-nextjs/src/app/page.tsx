3fd487cff74c81e2fa64c51c44f95873db71da0b
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2025-06-08 13:11:49 +1000

    retrieve dummy letters from supabase database

--- letters-unsent-nextjs/src/app/page.tsx
+++ letters-unsent-nextjs/src/app/page.tsx
@@ -1,95 +1,52 @@
-import Image from "next/image";
+/* 
+-------------------------------------------------------------------------------------------------
+
+  IMPORTS
+
+-------------------------------------------------------------------------------------------------
+*/
+
 import styles from "./page.module.css";
+import { createClient } from '../utils/supabase/server'
+import { cookies } from 'next/headers'
 
-export default function Home() {
+/* 
+-------------------------------------------------------------------------------------------------
+  
+  PURPOSE 
+  Root page of the site.
+  Displays all letters
+
+------------------------------------------------------------------------------------------------- 
+*/
+
+export default async function Home() {
+
+  const supabase = await createClient()
+
+  const { data: letters, error } = await supabase.from('letter').select()
+
+  if (error) {
+    console.error("Supabase error:", error)
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <ul>
+        {letters?.map((letter) => (
+          <li key={letter.id}>
+
+            <div>
+              <h2>{letter.intendedRecipient}</h2>
+              <p>{letter.created_at}</p>
+              <p>{letter.content}</p>
+            </div>
+          </li>
+          
+        ))}
+      </ul>
+
     </div>
   );
 }

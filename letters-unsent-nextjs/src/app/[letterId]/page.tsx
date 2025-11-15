610240f40690744384d0ad92ab0355f6a5f932b6
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2025-11-15 17:25:36 +1100

    chore: remove update button with no functionality

--- letters-unsent-nextjs/src/app/[letterId]/page.tsx
+++ letters-unsent-nextjs/src/app/[letterId]/page.tsx
@@ -18,11 +18,6 @@ export default async function LetterPage({
   return (
     <div>
         <NavBar />
-
-        <button>
-          Update letter
-        </button>
-
         <div>
             <h2>{data.letter[0].intended_recipient}</h2>
             <p>{data.letter[0].created_at}</p>

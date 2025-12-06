20f09b8f9cd923f82f495c431cd987634e2a60af
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2025-12-06 16:56:52 +1100

    style: convert timestamp into human readable format and adjust rendering dependent on null or empty strings

--- letters-unsent-nextjs/src/app/[letterId]/page.tsx
+++ letters-unsent-nextjs/src/app/[letterId]/page.tsx
@@ -1,4 +1,7 @@
-import NavBar from "@/components/NavBar"
+
+import { Suspense } from "react";
+import Spinner from "@/components/Spinner"
+import { convertDate } from "@/utils/functions"
 
 export default async function LetterPage({
   params,
@@ -8,27 +11,51 @@ export default async function LetterPage({
     const letterIdString = await params
 
     const letterId = Number(letterIdString.letterId)
-    console.log(typeof(letterId)) // this is confirmed to be a number
 
     const res = await fetch(`${process.env.SUPABASE_API_URL}/singleLetter?&letterId=${letterId}`)
+
     const data = await res.json()
 
-    console.log("letter return on client side: ", data)
- 
   return (
-    <div className="single-letter-container">
-      <div className="single-letter">
-          <p className="single-letter-date">
-            {data.letter[0].created_at}
-          </p>
-          <h2>
-            {data.letter[0].intended_recipient}
-          </h2> 
-          <p className="single-letter-content">
-            {data.letter[0].content}
-          </p>
-          <p>{data.letter[0].author_name}</p>
+    <>
+      <div className="single-letter-container">
+        <Suspense fallback={<Spinner />}>
+          <div className="single-letter">
+            <p className="single-letter-date">
+              {convertDate(data.letter[0].created_at)}
+            </p>
+
+            {/* alternate rendering depending on whether a recipient is named  */}
+
+            {
+              ( data.letter[0].intended_recipient === "" || data.letter[0].intended_recipient === null )? (
+                <p className="single-letter-content-no-recipient">
+                  {data.letter[0].content}
+                </p>
+              ) : (
+                <>
+                  <h2>
+                    {data.letter[0].intended_recipient}
+                  </h2> 
+                  <p className="single-letter-content">
+                    {data.letter[0].content}
+                  </p>
+                </>
+              )
+            }
+
+            {
+              ( data.letter[0].author_name === "" || data.letter[0].author_name === null ) ? (
+                <></>
+              ) : (
+                <p>{data.letter[0].author_name}</p>
+              )
+            }
+
+          </div>
+        </Suspense>
+        <p className="timestamp">{data.letter[0].created_at}</p>
       </div>
-    </div>
+    </>
   )
 }

02c0f82fd424f3b4bfb554470a5c69de7f372efe
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2025-12-24 15:02:27 +1100

    a11y: added tab title to assist with navigation

---
+++ letters-unsent-nextjs/src/app/submit/layout.tsx
@@ -0,0 +1,35 @@
+/* 
+-------------------------------------------------------------------------------------------------
+
+  IMPORTS
+
+-------------------------------------------------------------------------------------------------
+*/
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Letters Unsent—Release",
+  description: "Write and submit your own letter",
+};
+
+/* 
+-------------------------------------------------------------------------------------------------
+  
+  PURPOSE 
+  Defines layout across all pages of website
+
+------------------------------------------------------------------------------------------------- 
+*/
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+        {children}
+    </>
+  );
+}

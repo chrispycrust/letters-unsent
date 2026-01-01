d6561f834c5b4465bca65edc29879a6ac2b564df
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2026-01-01 12:02:34 +1100

    feat: add another optional field for name in bespoke contact component

--- letters-unsent-nextjs/src/components/ContactForm.tsx
+++ letters-unsent-nextjs/src/components/ContactForm.tsx
@@ -1,9 +1,20 @@
 export default function ContactForm() {
+
     return (
         <form
             method = "post"
             className="contact-form"
         >   
+            <label>
+                <strong>Name (optional)</strong>
+            </label>
+            <textarea
+                rows={1}
+            >
+            </textarea>
+
+            <br />
+
             <label>
                 <strong>What is your message about?</strong>
             </label>
@@ -13,8 +24,8 @@ export default function ContactForm() {
                 <option value="">
                     Please choose a category
                 </option>
-                <option value="Error">
-                    Error
+                <option value="Error/bug - please provide detailed description and/or attach screenshot of error/bug">
+                    Error/Bug
                 </option>
                 <option value="Dispute">
                     Content concern
@@ -35,9 +46,23 @@ export default function ContactForm() {
             >
             </textarea>
 
+             <>
+                <label>Attach a screenshot of the error or bug if needed</label>
+                <input type="file" accept="image/png, image/jpeg" />
+            </>
+
+            {
+                // if the option value is "error" or "bug" activate a section to attach a file of the error/bug
+                <>
+                    <label>Attach a screenshot of the error or bug if needed</label>
+                    <input type="file" accept="image/png, image/jpeg" />
+                </>
+            }
+
             <div>
                 <button
                     type="submit"
+                    // onSubmit={submitAnonymousMessage}
                 >
                     Send message
                 </button>

53420f954f1c7afa602fc576a192e99b5d43ddf7
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2026-01-03 11:28:41 +1100

    fix: calculate window inner width on page load to display appropriate navigation menu

--- letters-unsent-nextjs/src/components/NavBar.tsx
+++ letters-unsent-nextjs/src/components/NavBar.tsx
@@ -28,13 +28,19 @@ import { useEffect } from "react";
 export default function NavBar() {
 
   const [ showModal, setShowModal ] = useState(false)
-  const [ windowInnerWidth, setWindowInnerWidth ] = useState(window.innerWidth)
+
+  // initial null state for inner width of a window set to null to account for scenario where: 
+  // window hasn't been measured yet
+  // otherwise window.innerWidth will never resolve to null or undefined
+  const [ windowInnerWidth, setWindowInnerWidth ] = useState<number | null>(null)
+
 
   useEffect(() => {
-
       const handleResize = () => {
         setWindowInnerWidth(window.innerWidth)
       }
+
+      handleResize()
 
       window.addEventListener('resize', handleResize);
       
@@ -49,40 +55,42 @@ export default function NavBar() {
       <Link href="/">Letters Unsent</Link>
 
       {
-        windowInnerWidth < 550 ? (
+         windowInnerWidth === null ? null : ( // avoid flicker / wrong initial layout
+          windowInnerWidth < 550 ? (
 
-          <>
-            <button
-              type="button"
-              onClick={() => setShowModal(true)}
-              className="button-change-modal"
-            >
-              <EnvelopeClosedIcon />
-            </button>
+            <>
+              <button
+                type="button"
+                onClick={() => setShowModal(true)}
+                className="button-change-modal"
+              >
+                <EnvelopeClosedIcon />
+              </button>
 
-            {
-              showModal &&
-                <NavigationModal 
-                  onClose={() => setShowModal(false)} 
-                />
-            }
-          </>
+              {
+                showModal &&
+                  <NavigationModal 
+                    onClose={() => setShowModal(false)} 
+                  />
+              }
+            </>
 
-        ) : (
+          ) : (
 
-          <div className="nav-links">
-            <Link 
-              href="/submit" 
-            >
-              Release A Letter
-            </Link>
-            <Link 
-              href="/about" 
-            >
-              About & Contact
-            </Link>
-          </div>
+            <div className="nav-links">
+              <Link 
+                href="/submit" 
+              >
+                Release A Letter
+              </Link>
+              <Link 
+                href="/about" 
+              >
+                About & Contact
+              </Link>
+            </div>
 
+          )
         )
       }
 

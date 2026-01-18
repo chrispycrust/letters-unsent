99c7190c7454f68f02b1e49839565fb7d440df71
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2026-01-18 14:32:29 +1100

    build: integrate Sentry for error monitoring

---
+++ letters-unsent-nextjs/sentry.edge.config.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://92a39fb40f72009c0e3f4734c8bb4ad6@o4510729201057792.ingest.us.sentry.io/4510729202958336",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});

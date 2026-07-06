ab91ca8044ef3f04680ab85037b3f27b19d0abd0
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2026-07-06 17:30:00 +1000

    style: move navbar icon stroke width to CSS

--- letters-unsent-nextjs/public/icons/envelope-open.tsx
+++ letters-unsent-nextjs/public/icons/envelope-open.tsx
@@ -1,13 +1,13 @@
 export default function EnvelopeOpenIcon() {
     return (
         <svg 
+            className="button-change-modal-icon"
             xmlns="http://www.w3.org/2000/svg" 
             width="24" 
             height="24" 
             viewBox="0 0 24 24" 
             fill="none" 
             stroke="currentColor" 
-            strokeWidth="1" 
             strokeLinecap="round" 
             strokeLinejoin="round" 
             role="img"
